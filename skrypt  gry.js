ea823e9d3d124b0696b0c285ff06160e79753dce
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2023-06-14 17:53:14 +0200

    Add files via upload

---
+++ skrypt  gry.js
@@ -0,0 +1,38 @@
+GamepadButton.return(), innerWidth="1028", innerHeight="642"
+[1]2[3]3[4][5][6]7[7][8]8[9]9[6]52e[wds]CDATASection
+1[3[6[10--]]]15[21[
+IDBCursor,CSSTransition(CSS).HTMLMetaElement(www.youtube.com).define([
+        'require',
+        'dependency'
+    ], function(require, factory) {
+        'use strict';
+        high;
+        if eelse;
+    low
+        })= 'a' ='c'='d'='e'='i' = 'j' = 'k' = 'l' = 'm' = 'n' = new Promise((resolve, reject) => {
+            yx(22)<90)
+            alert;Text:('win')
+
+        }) 
+resizeTo(1028, 2024)
+
+
+        e#DE991-(   52109016360000000105498761 + )
+        'iszs'
+
+
+    wfffff\
+    31
+    2
+    1
+
+    2
+
+    2
+    222
+    3
+    4
+    5
+    lqksosPLPq,<onclick[IntersectionObserver]
+    Location;
+]]
